ce1143214fc52b9759e15b16c6d5a60a506dbd1d
JesusMejia3VC51 <75956851+JesusMejia3VC51@users.noreply.github.com>
2020-12-14 21:40:30 -0600

    Update CtrlSaludo.js

--- js/CtrlSaludo.js
+++ js/CtrlSaludo.js
@@ -7,4 +7,32 @@ throw new Error("Falta poner el nombre");
 }
 return `Hola ${nombreSinEspacios}:`;
 }
+calculaBoleta(boleta) {
+const boletaSinEspacios = boleta ? boleta.trim() : "";
+if (!boletaSinEspacios) {
+throw new Error("Falta la boleta");
+}
+return `Tu boleta es: ${boletaSinEspacios}:`;
+}
+calculaGrupo(grupo) {
+const grupoSinEspacios = grupo ? grupo.trim() : "";
+if (!grupoSinEspacios) {
+throw new Error("Falta poner el grupo");
+}
+return `Tu grupo es: ${grupoSinEspacios}:`;
+}
+calculaMateria(materia) {
+const materiaSinEspacios = materia ? materia.trim() : "";
+if (!materiaSinEspacios) {
+throw new Error("Falta poner la materia");
+}
+return `Tu materia es: ${materiaSinEspacios}:`;
+}
+calculaFecha(fecha) {
+const fechaSinEspacios = fecha ? fecha.trim() : "";
+if (!fechaSinEspacios) {
+throw new Error("Falta poner la fehca");
+}
+return `La fecha es: ${fechaSinEspacios}:`;
+}
 }
